6bbecc3d3a99ecad9ac6bbcf2f08bb626c550f96
maha <mmahaece07@gmail.com>
2022-01-10 17:03:31 +0530

    Make My Own Project

---
+++ ADO.NET Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADO.NET_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
